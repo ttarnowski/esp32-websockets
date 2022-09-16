70bf287b06873f5b6e10bfa6953fc3880db08ef1
Tomasz Tarnowski <t.tarnowski90@gmail.com>
2022-09-16 11:29:52 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Control ESP32 from anywhere in the World - ESP32 PlatformIO Project
+
+This repository has been created as a part of the YouTube video:
+[Control ESP32 from ANYWHERE in the World - Step-By-Step Tutorial](https://youtu.be/z53MkVFOnIo)
+
+This is ESP32 Arduino Framework Code that uses WebSocket client to connect to [AWS API Gateway WebSockets](https://github.com/ttarnowski/esp32-websockets-serverless-handler) that acts as an intermediary between ReactJS Web Application and ESP32 Microcontroller.
+
+## Prerequisites
+
+- Visual Studio Code with PlatformIO extension installed
+
+## Licence
+
+MIT.
---
+++ src/main.cpp
@@ -0,0 +1,164 @@
+#include <Arduino.h>
+#include <WebSocketsClient.h>
+#include <WiFiMulti.h>
+
+#include <ArduinoJson.h>
+
+#define WIFI_SSID "your_wifi_network_name"
+#define WIFI_PASSWORD "your_wifi_password"
+
+#define WS_HOST "host.to.your.aws.api.gateway.websockets"
+#define WS_PORT 443
+#define WS_URL "/dev"
+
+#define JSON_DOC_SIZE 2048
+#define MSG_SIZE 256
+
+WiFiMulti wifiMulti;
+WebSocketsClient wsClient;
+
+void sendErrorMessage(const char *error) {
+  char msg[MSG_SIZE];
+
+  sprintf(msg, "{\"action\":\"msg\",\"type\":\"error\",\"body\":\"%s\"}",
+          error);
+
+  wsClient.sendTXT(msg);
+}
+
+void sendOkMessage() {
+  wsClient.sendTXT("{\"action\":\"msg\",\"type\":\"status\",\"body\":\"ok\"}");
+}
+
+void sendPinChangeMessage(uint8_t pin, int value) {
+  char msg[MSG_SIZE];
+
+  sprintf(msg,
+          "{\"action\":\"msg\",\"type\":\"pinChange\",\"body\":{\"pin\":%d,"
+          "\"value\":%d}}",
+          pin, value);
+
+  wsClient.sendTXT(msg);
+}
+
+uint8_t toMode(const char *val) {
+  if (strcmp(val, "output") == 0) {
+    return OUTPUT;
+  }
+
+  if (strcmp(val, "input_pullup") == 0) {
+    return INPUT_PULLUP;
+  }
+
+  return INPUT;
+}
+
+void handleMessage(uint8_t *payload) {
+  StaticJsonDocument<JSON_DOC_SIZE> doc;
+
+  DeserializationError error = deserializeJson(doc, payload);
+
+  // Test if parsing succeeds.
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    sendErrorMessage(error.c_str());
+    return;
+  }
+
+  if (!doc["type"].is<const char *>()) {
+    sendErrorMessage("invalid message type format");
+    return;
+  }
+
+  if (strcmp(doc["type"], "cmd") == 0) {
+    if (!doc["body"].is<JsonObject>()) {
+      sendErrorMessage("invalid command body");
+      return;
+    }
+
+    if (strcmp(doc["body"]["type"], "pinMode") == 0) {
+      /*
+      Uncomment this code for better validation of pinMode command
+
+      if (!doc["body"]["mode"].is<const char *>()) {
+        sendErrorMessage("invalid pinMode mode type");
+        return;
+      }
+
+      if (strcmp(doc["body"]["mode"], "input") != 0 &&
+          strcmp(doc["body"]["mode"], "input_pullup") != 0 &&
+          strcmp(doc["body"]["mode"], "output") != 0) {
+        sendErrorMessage("invalid pinMode mode value");
+        return;
+      }
+      */
+
+      pinMode(doc["body"]["pin"], toMode(doc["body"]["mode"]));
+      sendOkMessage();
+      return;
+    }
+
+    if (strcmp(doc["body"]["type"], "digitalWrite") == 0) {
+      digitalWrite(doc["body"]["pin"], doc["body"]["value"]);
+      sendOkMessage();
+      return;
+    }
+
+    if (strcmp(doc["body"]["type"], "digitalRead") == 0) {
+      auto value = digitalRead(doc["body"]["pin"]);
+
+      char msg[MSG_SIZE];
+
+      sprintf(msg, "{\"action\":\"msg\",\"type\":\"output\",\"body\":%d}",
+              value);
+
+      wsClient.sendTXT(msg);
+      return;
+    }
+
+    sendErrorMessage("unsupported command type");
+    return;
+  }
+
+  sendErrorMessage("unsupported message type");
+  return;
+}
+
+void onWSEvent(WStype_t type, uint8_t *payload, size_t length) {
+  switch (type) {
+  case WStype_CONNECTED:
+    Serial.println("WS Connected");
+    break;
+  case WStype_DISCONNECTED:
+    Serial.println("WS Disconnected");
+    break;
+  case WStype_TEXT:
+    Serial.printf("WS Message: %s\n", payload);
+
+    handleMessage(payload);
+
+    break;
+  }
+}
+
+void setup() {
+  Serial.begin(921600);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+
+  while (wifiMulti.run() != WL_CONNECTED) {
+    delay(100);
+  }
+
+  Serial.println("Connected");
+
+  wsClient.beginSSL(WS_HOST, WS_PORT, WS_URL, "", "wss");
+  wsClient.onEvent(onWSEvent);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, WiFi.status() == WL_CONNECTED);
+  wsClient.loop();
+}
